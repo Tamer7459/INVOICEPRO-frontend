4ad9d82f44e19183f4e2c94ae0d8ae2a0731a680
Tamer <tamerinale@gmail.com>
2026-06-20 16:27:30 +0100

    final project

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
   }, [router, searchParams]);
 
   const handleGoogleLogin = () => {
-    window.location.href = "/api/auth/google";
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/google`;
   };
 
   const handleDemoLogin = async () => {
